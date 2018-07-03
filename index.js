c377f23ae0786d976f1200610ee1dda6cc70f39b
Lynn Steinmeyer <30458735+lynsheep@users.noreply.github.com>
2018-07-03 02:14:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers! Please message speehs#8885 for reporting bugs. Feedback is appreciated, thank you! ;)                                                                                                                                                                                                
+ `);
+    bot.user.setActivity("everything", {type: "LISTENING"});
+  });
+
