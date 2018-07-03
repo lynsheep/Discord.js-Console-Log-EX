d214e336518bf142f64e0906a3e352019cd4a393
Lynn Steinmeyer <30458735+lynsheep@users.noreply.github.com>
2018-07-03 02:15:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers! Please message speehs#8885 for reporting bugs. Feedback is appreciated, thank you! ;)                                                                                                                                                                                                
+    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers! example                                                                                                                                                                                                
  `);
     bot.user.setActivity("everything", {type: "LISTENING"});
   });
